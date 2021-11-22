9af5ecc0e63057169c2ac78685e9edadeb14d0d1
KanavN <89537335+KanavN@users.noreply.github.com>
2021-11-22 12:33:01 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,40 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+  attach(body){
+      this.sling.bodyA=body;
+  }
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            if(pointA.x<220){
+                strokeWeight(7);
+            stroke(48,22,8);
+            line(pointA.x-20, pointA.y, pointB.x-10, pointB.y);
+            line(pointA.x-20, pointA.y, pointB.x+30, pointB.y-3);
+            image(sling3,pointA.x-30,pointA.y-10,15,30)
+            }
+            else{
+            strokeWeight(4);
+            stroke(48,22,8);
+            line(pointA.x+25, pointA.y, pointB.x-10, pointB.y+10);
+            line(pointA.x+25, pointA.y, pointB.x+30, pointB.y+10);
+            image(sling3,pointA.x+20,pointA.y,15,30)
+        }
+    }
+  } 
+}
